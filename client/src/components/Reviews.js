fada052c027f316bb4eb972e0f940a4c92ae2036
othmaneuw <strangeothmane@gmail.com>
2023-05-30 15:16:01 +0100

    displaying valid reviews

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,7 +1,10 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import { UserContext } from "../context/UserContext";
 
 const Reviews = ({ trip }) => {
+  const { user } = useContext(UserContext);
   const [reviews, setReviews] = useState([]);
   const [showReviews, setShowReviews] = useState(false);
   const [addReview, setAddReview] = useState(false);
@@ -11,8 +14,7 @@ const Reviews = ({ trip }) => {
   useEffect(() => {
     axios.get("/reviews").then((response) => {
       const validReviews = response.data.filter(
-        (review) =>
-          (review.status === "valid" && review.trip._id === trip._id)
+        (review) => review.status === "valid" && review.trip._id === trip._id
       );
       setReviews(validReviews);
       console.log(validReviews);
@@ -88,11 +90,52 @@ const Reviews = ({ trip }) => {
         <div className="mt-5">
           <div className="bg-gray-700 p-8 rounded-xl">
             <h3 className="text-xl text-center">Reviews</h3>
-            {reviews.length > 0 && reviews.map(review =>(
-              <div>
-                {review.body}
-              </div>
-            ))}
+            {reviews.length > 0 &&
+              reviews.map((review) => (
+                <div className="bg-gray-500 flex p-4 rounded-xl mt-6 gap-4 items-center">
+                  <span className="bg-black p-3 rounded-full flex gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {review.user.name}
+                  </span>
+                  <div className="bg-green-500 flex gap-80 py-6 px-6 rounded-full">
+                    <p>{review.body}</p>
+                    <span className="text-sm text-gray-800">
+                      {formatDistanceToNow(new Date(review.createdAt), {
+                        addSuffix: true,
+                      })}
+                    </span>
+                  </div>
+                  {user.name === review.user.name && (
+                    <button className="bg-red-500 p-2 rounded-full">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  )}
+                </div>
+              ))}
           </div>
         </div>
       )}
